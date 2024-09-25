5f46308803044363918c4c5dc40aae9be975b541
Nithin <nithinpaddyur@gmial.com>
2024-09-25 20:30:59 +0530

    first Commit

---
+++ src/test/java/com/nithin/tests/StandAloneTest.java
@@ -0,0 +1,20 @@
+package com.nithin.tests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StandAloneTest {
+
+	@Test
+	public void loginTest() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+	}
+
+}
